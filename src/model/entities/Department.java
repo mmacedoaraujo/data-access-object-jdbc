0c2104569a30616c15c66d33fee2d685d5cda10b
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-07-12 13:30:15 -0300

    implemented department entity class

---
+++ src/model/entities/Department.java
@@ -0,0 +1,52 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String name;
+
+	public Department() {
+
+	}
+
+	public Department(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
